ea8d7358bacb44b09af6a9d00ea3c99cab4484b1
vishwas1 <vishwasanand.cse@gmail.com>
2024-05-14 16:59:26 +0530

    verifed signature of ed25519

--- rust/src/ed25519_test.rs
+++ rust/src/ed25519_test.rs
@@ -4,19 +4,45 @@ pub mod ed25519_test {
     use ed25519_dalek::{ Signature};
     use ed25519_dalek::{ SECRET_KEY_LENGTH, KEYPAIR_LENGTH,  };
     use ed25519_dalek::VerifyingKey;
-    use multibase::{ decode };
+    use json_ld::print;
     use ed25519_dalek::PUBLIC_KEY_LENGTH;
     use serde_json::{ json, Value };
-    
+    use sha256::digest;
+    use ed25519_dalek::{Verifier};
+    // use multibase::{encode, decode, Base};
+    use ed25519_dalek::{DigestSigner};
+    use bincode::deserialize;
+    use base58::ToBase58;
+    use multibase::{encode, Base};
+
+    use didkit::ssi::{
+        self,
+        jsonld::{parse_ld_context, StaticLoader},
+        rdf,
+        jwk::{Algorithm, Error}
+    };
+
     struct Ed25519KeyPair {
         signing_key: SigningKey,
         verifying_key: VerifyingKey,
     }
 
     fn decode_multibase_public_key(multibase_str: &str) -> Result<Vec<u8>, String> {
-        let decoded = decode(multibase_str).unwrap();
-        let (_, data) = decoded;
-        Ok(data.to_vec())
+        let decoded = multibase::decode(multibase_str).unwrap();
+        let (base, data) = decoded;
+        match base {
+            Base::Base58Btc => {
+                // `data` is the decoded byte array
+                // println!("Decoded data (Base58btc): {:?}", data);
+                //println!("Decoded data (Base58btc) vec: {:?}", data.to_vec());
+            }
+            _ => {
+                println!("Unsupported base: {:?}", base);
+            }
+        }
+
+        Ok(data)
+
     }
 
     fn vec_to_array<const N: usize>(input: Vec<u8>) -> Result<[u8; N], &'static str> {
@@ -33,8 +59,8 @@ pub mod ed25519_test {
     }
 
     fn generate_key(secret_key_str: &str, public_key_str: &str) -> Ed25519KeyPair {
-        //let secret_key = "zrv4gLTkvUgXPWok7r71vTzcmF5tLuPGzzGL5ZzEQwySaCBmKJerJUSNGM8peNMfhqfkXQXBM2S6JSiCUuvw1W9ziQ5";
         let secret_key_bytes = decode_multibase_public_key(secret_key_str).unwrap();
+        println!("secret_key_byteslen : {}", secret_key_bytes.len());
 
         const TOTAL_LENGTH: usize = KEYPAIR_LENGTH + 2;
         let secret_key_array = vec_to_array::<TOTAL_LENGTH>(secret_key_bytes.to_owned()).unwrap();
@@ -51,13 +77,14 @@ pub mod ed25519_test {
             .try_into()
             .expect("Failed to create new array");
         let mut signing_key: SigningKey = SigningKey::from_bytes(&secret_key_arry);
+        
 
         const ARRAY_LENGTH: usize = 34;
-        //let public_key_str = "z6MkmX4KyqzfM4WGUVLFTtD2dVbyF5HmZwRrgWwkHLLgrCnH";
         let public_key_bytes = decode_multibase_public_key(public_key_str).unwrap();
+        println!("public_key_bytes len : {}", public_key_bytes.len());
         let t_public_key_array = vec_to_array::<ARRAY_LENGTH>(public_key_bytes.to_owned()).unwrap();
 
-        // extract secret key from index 2 to 32
+        //extract secret key from index 2 to 32
         let public_key_start_index = 2;
         let public_key_end_index = t_public_key_array.len();
         let public_key_array: [u8; 32] = t_public_key_array[
@@ -65,7 +92,6 @@ pub mod ed25519_test {
         ]
             .try_into()
             .expect("Failed to create new array");
-
         let public_key_bytes: [u8; PUBLIC_KEY_LENGTH] = public_key_array;
         let verifying_key = VerifyingKey::from_bytes(&public_key_bytes).unwrap();
 
@@ -75,32 +101,72 @@ pub mod ed25519_test {
         };
     }
     
-    pub fn test(public_key_str: &str, m: &str, signature_str: &str) {
-        let secret_key =
-            "zrv4gLTkvUgXPWok7r71vTzcmF5tLuPGzzGL5ZzEQwySaCBmKJerJUSNGM8peNMfhqfkXQXBM2S6JSiCUuvw1W9ziQ5";
-        
-        let mut key_pair: Ed25519KeyPair = generate_key(&secret_key, &public_key_str);
 
+
+
+    fn generate_verifying_key(public_key_str: &str) -> VerifyingKey {
+        const ARRAY_LENGTH: usize = 34;
+        let public_key_bytes = decode_multibase_public_key(public_key_str).unwrap();
+        let t_public_key_array = vec_to_array::<ARRAY_LENGTH>(public_key_bytes.to_owned()).unwrap();
+        println!("t_public_key_array.len {:?}",t_public_key_array.len());
+
+        // extract secret key from index 2 to 32
+        let public_key_start_index = 2;
+        let public_key_end_index = t_public_key_array.len();
+        let public_key_array: [u8; 32] = t_public_key_array[
+            public_key_start_index..public_key_end_index
+        ]
+            .try_into()
+            .expect("Failed to create new array");
+        let public_key_bytes: [u8; PUBLIC_KEY_LENGTH] = public_key_array;
+        println!("public_key_bytes.len {:?}",public_key_bytes.len());
+
+        let verifying_key = ed25519_dalek::VerifyingKey::from_bytes(&public_key_bytes).unwrap();
+        // let verifying_key =  VerifyingKey::from(&public_key_bytes);
+        // println!("verifying_key.len {:?}",verifying_key);
+
+
+        return verifying_key
+    }
+
+    // pub fn test1(encoded_verifying_key: &str, encoded_signature: &str)->bool{
+
+
+        
+    //     let message: &[u8] = b"53fb655cc23c4fccd5dba9d8be70bc46662cd7aaf7c22f5cf843f2919aaffd64290c8b14409f78bf6aab4aad985e7c5524bc2e7bf1f7663404ea628576ac4119";
+    //     let decoded_verifying_key: VerifyingKey = deserialize(&encoded_verifying_key.as_bytes()).unwrap();
+    //     let decoded_signature: Signature = deserialize(&encoded_signature.as_bytes()).unwrap();
+    //     let verified: bool = decoded_verifying_key.verify(&message, &decoded_signature).is_ok();
+    //     println!("Result {:?} ", verified);
+    //     verified
+    // }
+
+    pub fn test(public_key_str: &str,  m:  &str, signature_str1: &str, algorithm: Algorithm, private_key_str: &str) {
+        
         // message
-        let message: &[u8] = m.as_bytes();
+        // m = "abcd";
+        let hex_decode_message = hex::decode(m);
+        let message: &[u8] = &hex_decode_message.unwrap();
     
         // sign message using privaye key
+        // let secret_key = private_key_str;        
+        // let mut key_pair: Ed25519KeyPair = generate_key(&secret_key, &public_key_str);
         // let signature: Signature = key_pair.signing_key.sign(message);
-        // println!("signature {:?}", signature.to_string());
-
-        println!("signature_str {:?}", signature_str);
+        // let signature_bytes = &signature.to_bytes();
+        // let signature_hex = hex::encode(signature_bytes);
+        // println!("Hex_sig: {}", signature_hex);
+        // let base58_string = signature_bytes.to_base58();
+        // println!("signature_multibase_encoded z{}", base58_string.to_string()); 
+        
         const SIGNATURE_BYTE_SIZE: usize = 64;
-        let signature_bytes = decode_multibase_public_key(signature_str).unwrap();
-        let signature_array = vec_to_array::<SIGNATURE_BYTE_SIZE>(signature_bytes.to_owned()).unwrap();
-        println!("signature_str_len {:?}", signature_array.len());
-        let signature_start_index = 0;
-        let signature_end_index = signature_array.len();
-        let signature_array: [u8; SIGNATURE_BYTE_SIZE] = signature_array[
-            signature_start_index..signature_end_index
-        ].try_into().expect("Failed to extract signature");
+        let signature_bytes = decode_multibase_public_key(signature_str1).unwrap();
+        println!("signature_bytes {:?}", signature_bytes.len()); 
+        let signature_array = vec_to_array::<SIGNATURE_BYTE_SIZE>(signature_bytes.to_owned()).unwrap();        
+        println!("signature_str1_len {:?}", signature_array.len());
         let signature: Signature = Signature::from_bytes(&signature_array);
-         
-        let res1 = key_pair.verifying_key.verify_strict(&message, &signature);
-        println!("result1 {:?}", res1.is_ok());
+        let verifying_key: ed25519_dalek::VerifyingKey = generate_verifying_key(&public_key_str);
+
+        let res1 = verifying_key.verify(&message, &signature).is_ok();
+        println!("result1 {:?}", res1);
     }
 }
